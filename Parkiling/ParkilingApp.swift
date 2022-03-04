370a8971fe89a6074b46741b86e89e6fe82dddc9
Nicholas <nichz123@gmail.com>
2022-03-04 22:05:24 +0700

    Initial Commit

---
+++ Parkiling/ParkilingApp.swift
@@ -0,0 +1,20 @@
+//
+//  ParkilingApp.swift
+//  Parkiling
+//
+//  Created by Nicholas on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkilingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
